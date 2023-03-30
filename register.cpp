c2393a0362a0005f828ffdb115cf5db0f2573576
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-03-30 11:53:13 +0700

    Update register.cpp

--- register.cpp
+++ register.cpp
@@ -1,45 +1,65 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class User {
-    private:
-        string username;
-        string password;
-    public:
-        User() {}
-        User(string u, string p) {
-            username = u;
-            password = p;
+private:
+    string username;
+    string password;
+public:
+    User(string username, string password) {
+        this->username = username;
+        this->password = password;
+    }
+
+    string getUsername() {
+        return this->username;
+    }
+    string getPassword() {
+        return this->password;
+    }
+};
+
+class Authorization {
+public:
+    void registerUser(string username, string password) {
+        User user(username, password);
+            users.push_back(user);
+    }
+
+    bool login(string username, string password) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users[i].getUsername() == username && users[i].getPassword() == password) {
+                return true;
+            }
         }
-        string getUsername() {
-            return username;
-        }
-        string getPassword() {
-            return password;
-        }
-        void setUsername(string u) {
-            username = u;
-        }
-        void setPassword(string p) {
-            password = p;
-        }
+        return false;
+    }
+    
+private:
+    vector<User>users;
 };
 
 int main() {
-    User user;
-    string inputUsername, inputPassword;
+    Authorization auth;
+    auth.registerUser("user1", "password1");
+    auth.registerUser("user2", "password2");
 
+    string username, password;
     cout << "Пожалуйста, введите Ваше имя: ";
-    cin >> inputUsername;
-    user.setUsername(inputUsername);
-
+    cin >> username;
     cout << "Пожалуйста, введите Ваш пароль: ";
-    cin >> inputPassword;
-    user.setPassword(inputPassword);
+    cin >> password;
 
-    cout << "\nРегистрация завершена. Добро пожаловать, " << user.getUsername() << endl;
+    if (auth.login(username, password)) {
+        cout << "Логин верен" << endl;
+    }
+    else {
+        cout << "Логин неверен" << endl;
+    }
 
     return 0;
 }
+
